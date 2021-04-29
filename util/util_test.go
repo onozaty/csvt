31046cc3616d4a1ce9e568dd026b0188c98918e7
onozaty <onozaty@gmail.com>
2021-04-29 23:14:37 +0900

    [join] ユニットテスト追加

---
+++ util/util_test.go
@@ -0,0 +1,68 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIndexOf(t *testing.T) {
+
+	strings := []string{"a", "b", "c"}
+
+	index := IndexOf(strings, "a")
+	if index != 0 {
+		t.Fatal("failed test\n", index)
+	}
+
+	index = IndexOf(strings, "b")
+	if index != 1 {
+		t.Fatal("failed test\n", index)
+	}
+
+	index = IndexOf(strings, "c")
+	if index != 2 {
+		t.Fatal("failed test\n", index)
+	}
+
+	index = IndexOf(strings, "d")
+	if index != -1 {
+		t.Fatal("failed test\n", index)
+	}
+}
+
+func TestIndexOf_multi(t *testing.T) {
+
+	strings := []string{"a", "a", "a"}
+
+	// 最初に見つかった位置になること
+	index := IndexOf(strings, "a")
+	if index != 0 {
+		t.Fatal("failed test\n", index)
+	}
+}
+
+func TestRemove(t *testing.T) {
+
+	removed := Remove([]string{"a", "b", "c"}, "a")
+	if !reflect.DeepEqual(removed, []string{"b", "c"}) {
+		t.Fatal("failed test\n", removed)
+	}
+
+	removed = Remove([]string{"a", "b", "c"}, "b")
+	if !reflect.DeepEqual(removed, []string{"a", "c"}) {
+		t.Fatal("failed test\n", removed)
+	}
+
+	removed = Remove([]string{"a", "b", "c"}, "c")
+	if !reflect.DeepEqual(removed, []string{"a", "b"}) {
+		t.Fatal("failed test\n", removed)
+	}
+}
+
+func TestRemove_multi(t *testing.T) {
+
+	removed := Remove([]string{"a", "b", "c", "b"}, "b")
+	if !reflect.DeepEqual(removed, []string{"a", "c"}) {
+		t.Fatal("failed test\n", removed)
+	}
+}
