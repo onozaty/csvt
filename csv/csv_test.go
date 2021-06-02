dbaed234332397a718a7efe91199fc90b96444a7
onozaty <onozaty@gmail.com>
2021-06-02 22:58:36 +0900

    CsvReaderでLF、CRLFのどちらでもレコードの区切りとして扱われることを確認するテスト追加

--- csv/csv_test.go
+++ csv/csv_test.go
@@ -76,6 +76,45 @@ func TestNewCsvReader_withoutBOM(t *testing.T) {
 	}
 }
 
+func TestNewCsvReader_LF_CRLF(t *testing.T) {
+
+	s := "ID,Name\n1,Yamada\r\n2,Ichikawa"
+
+	r, err := NewCsvReader(strings.NewReader(s))
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	header, err := r.Read()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	if !reflect.DeepEqual(header, []string{"ID", "Name"}) {
+		t.Fatal("failed test\n", header)
+	}
+
+	firstRow, err := r.Read()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	if !reflect.DeepEqual(firstRow, []string{"1", "Yamada"}) {
+		t.Fatal("failed test\n", header)
+	}
+
+	secondRow, err := r.Read()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+	if !reflect.DeepEqual(secondRow, []string{"2", "Ichikawa"}) {
+		t.Fatal("failed test\n", header)
+	}
+
+	_, err = r.Read()
+	if err != io.EOF {
+		t.Fatal("failed test\n", err)
+	}
+}
 func TestNewCsvWriter(t *testing.T) {
 
 	var b bytes.Buffer
