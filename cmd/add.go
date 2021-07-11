5be540afb4c547e25b473f6445fddfcb148e5ea7
onozaty <onozaty@gmail.com>
2021-07-11 22:53:37 +0900

    [add] addコマンド実装 #11

---
+++ cmd/add.go
@@ -0,0 +1,184 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"text/template"
+
+	"github.com/onozaty/csvt/csv"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+func newAddCmd() *cobra.Command {
+
+	addCmd := &cobra.Command{
+		Use:   "add",
+		Short: "Add column",
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			format, err := getFlagBaseCsvFormat(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			inputPath, _ := cmd.Flags().GetString("input")
+			addColumnName, _ := cmd.Flags().GetString("column")
+			staticValue, _ := cmd.Flags().GetString("value")
+			templString, err := getFlagEscapedString(cmd.Flags(), "template")
+			if err != nil {
+				// エスケープを戻す際にエラーになる場合があるのでハンドリング
+				return err
+			}
+			copyColumnName, _ := cmd.Flags().GetString("copy-column")
+			outputPath, _ := cmd.Flags().GetString("output")
+
+			specifyValueCount := 0
+			if staticValue != "" {
+				specifyValueCount++
+			}
+			if templString != "" {
+				specifyValueCount++
+			}
+			if copyColumnName != "" {
+				specifyValueCount++
+			}
+			if specifyValueCount >= 2 {
+				return fmt.Errorf("not allowed to specify both --value and --template and --copy-column")
+			}
+
+			var templ *template.Template = nil
+
+			if templString != "" {
+				templ, err = template.New("template").Parse(templString)
+				if err != nil {
+					return errors.WithMessage(err, "--template is invalid")
+				}
+			}
+
+			// 引数の解析に成功した時点で、エラーが起きてもUsageは表示しない
+			cmd.SilenceUsage = true
+
+			return runAdd(
+				format,
+				inputPath,
+				addColumnName,
+				outputPath,
+				AddOptions{
+					staticValue:    staticValue,
+					template:       templ,
+					copyColumnName: copyColumnName,
+				})
+		},
+	}
+
+	addCmd.Flags().StringP("input", "i", "", "Input CSV file path.")
+	addCmd.MarkFlagRequired("input")
+	addCmd.Flags().StringP("column", "c", "", "Name of the column to add.")
+	addCmd.MarkFlagRequired("column")
+	addCmd.Flags().StringP("value", "", "", "(optional) Fixed value to set for the added column.")
+	addCmd.Flags().StringP("template", "", "", "(optional) Template for the value to be set for the added column.")
+	addCmd.Flags().StringP("copy-column", "", "", "(optional) Name of the column from which the value is copied.")
+	addCmd.Flags().StringP("output", "o", "", "Output CSV file path.")
+	addCmd.MarkFlagRequired("output")
+
+	return addCmd
+}
+
+type AddOptions struct {
+	staticValue    string
+	template       *template.Template
+	copyColumnName string
+}
+
+func runAdd(format csv.Format, inputPath string, addColumnName string, outputPath string, options AddOptions) error {
+
+	reader, writer, close, err := setupInputOutput(inputPath, outputPath, format)
+	if err != nil {
+		return err
+	}
+	defer close()
+
+	err = add(reader, addColumnName, writer, options)
+
+	if err != nil {
+		return err
+	}
+
+	return writer.Flush()
+}
+
+func add(reader csv.CsvReader, addColumnName string, writer csv.CsvWriter, options AddOptions) error {
+
+	// ヘッダ
+	columnNames, err := reader.Read()
+	if err != nil {
+		return errors.Wrap(err, "failed to read the CSV file")
+	}
+
+	copyColumnIndex := -1
+	if options.copyColumnName != "" {
+		copyColumnIndex, err = getTargetColumnIndex(columnNames, options.copyColumnName)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = writer.Write(append(columnNames, addColumnName))
+	if err != nil {
+		return err
+	}
+
+	// ヘッダ以外
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "failed to read the CSV file")
+		}
+
+		addColumnValue := ""
+		if options.staticValue != "" {
+
+			addColumnValue = options.staticValue
+
+		} else if options.template != nil {
+
+			addColumnValue, err = executeTemplate(options.template, columnNames, row)
+			if err != nil {
+				return err
+			}
+
+		} else if options.copyColumnName != "" {
+
+			addColumnValue = row[copyColumnIndex]
+		}
+
+		err = writer.Write(append(row, addColumnValue))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func executeTemplate(templ *template.Template, columnNames []string, row []string) (string, error) {
+
+	// テンプレートにはヘッダ名をキーとしたmapで
+	rowMap := make(map[string]string)
+	for i, columnName := range columnNames {
+		rowMap[columnName] = row[i]
+	}
+
+	w := new(strings.Builder)
+	err := templ.Execute(w, rowMap)
+	if err != nil {
+		return "", err
+	}
+
+	return w.String(), nil
+}
---
+++ cmd/add_test.go
@@ -0,0 +1,472 @@
+package cmd
+
+import (
+	"os"
+	"testing"
+)
+
+func TestAddCmd(t *testing.T) {
+
+	s := joinRows(
+		"col1,col2",
+		"1,a",
+		"2,b",
+		"3,c",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+	})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	result := readString(t, fo)
+
+	expect := joinRows(
+		"col1,col2,col3",
+		"1,a,",
+		"2,b,",
+		"3,c,",
+	)
+
+	if result != expect {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestAddCmd_value(t *testing.T) {
+
+	s := joinRows(
+		"col1,col2",
+		"1,a",
+		"2,b",
+		"3,c",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--value", "x",
+	})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	result := readString(t, fo)
+
+	expect := joinRows(
+		"col1,col2,col3",
+		"1,a,x",
+		"2,b,x",
+		"3,c,x",
+	)
+
+	if result != expect {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestAddCmd_copyColumn(t *testing.T) {
+
+	s := joinRows(
+		"col1,col2",
+		"1,a",
+		"2,b",
+		"3,c",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--copy-column", "col1",
+	})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	result := readString(t, fo)
+
+	expect := joinRows(
+		"col1,col2,col3",
+		"1,a,1",
+		"2,b,2",
+		"3,c,3",
+	)
+
+	if result != expect {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestAddCmd_template(t *testing.T) {
+
+	s := joinRows(
+		"col1,col2",
+		"1,a",
+		"2,b",
+		"3,c",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--template", "{{.col1}}-{{.col2}}",
+	})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	result := readString(t, fo)
+
+	expect := joinRows(
+		"col1,col2,col3",
+		"1,a,1-a",
+		"2,b,2-b",
+		"3,c,3-c",
+	)
+
+	if result != expect {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestAddCmd_format(t *testing.T) {
+
+	s := joinRows(
+		"col1\tcol2",
+		"1\ta",
+		"2\tb",
+		"3\tc",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--template", "{{ if eq .col1 \"2\" }}{{ .col1 }}{{ else }}{{ .col2 }}{{ end }}",
+		"--delim", "\t",
+	})
+
+	err := rootCmd.Execute()
+	if err != nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	result := readString(t, fo)
+
+	expect := joinRows(
+		"col1\tcol2\tcol3",
+		"1\ta\ta",
+		"2\tb\t2",
+		"3\tc\tc",
+	)
+
+	if result != expect {
+		t.Fatal("failed test\n", result)
+	}
+}
+
+func TestAddCmd_invalidFormat(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--encoding", "aa",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != "invalid encoding name: aa" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_templateParseError(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--template", "{{.col1}", // 閉じ括弧が少ない
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != `--template is invalid: template: template:1: unexpected "}" in operand` {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_templateUnquoteError(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--template", `\n"`, // \が含まれる状態で、エスケープされていないバックスラッシュ
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != `Could not parse value \n" of flag template: invalid syntax` {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_value_template(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--value", "a",
+		"--template", "{{.col1}}",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != "not allowed to specify both --value and --template and --copy-column" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_value_copyColumn(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--value", "a",
+		"--copy-column", "col1",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != "not allowed to specify both --value and --template and --copy-column" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_template_copyColumn(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--template", "{{.col1}}",
+		"--copy-column", "col1",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != "not allowed to specify both --value and --template and --copy-column" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_inputFileNotFound(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi + "____", // 存在しないファイル
+		"-o", fo,
+		"-c", "col3",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil {
+		t.Fatal("failed test\n", err)
+	}
+
+	pathErr := err.(*os.PathError)
+	if pathErr.Path != fi+"____" || pathErr.Op != "open" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_copyColumnNotFound(t *testing.T) {
+
+	s := joinRows(
+		"col1,col2",
+		"1,a",
+		"2,b",
+		"3,c",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--copy-column", "col3",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != "missing col3 in the CSV file" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_inputFileEmpty(t *testing.T) {
+
+	fi := createTempFile(t, "")
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--copy-column", "col1",
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != "failed to read the CSV file: EOF" {
+		t.Fatal("failed test\n", err)
+	}
+}
+
+func TestAddCmd_templateExecuteError(t *testing.T) {
+
+	s := joinRows(
+		"col1,col2",
+		"1,a",
+		"2,b",
+		"3,c",
+	)
+
+	fi := createTempFile(t, s)
+	defer os.Remove(fi)
+
+	fo := createTempFile(t, "")
+	defer os.Remove(fo)
+
+	rootCmd := newRootCmd()
+	rootCmd.SetArgs([]string{
+		"add",
+		"-i", fi,
+		"-o", fo,
+		"-c", "col3",
+		"--template", "{{if eq 1 \"1\"}}xx{{end}}", // eqメソッドの引数の型がアンマッチで実行時にエラー
+	})
+
+	err := rootCmd.Execute()
+	if err == nil || err.Error() != `template: template:1:5: executing "template" at <eq 1 "1">: error calling eq: incompatible types for comparison` {
+		t.Fatal("failed test\n", err)
+	}
+}
